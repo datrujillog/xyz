d0f20dbf7102f9c83ac652f2c0b1465d8fe656a6
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-22 16:47:20 -0500

    feat(model-sale): Added Sale model to represent product sales

---
+++ database/models/sales.Model.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    const Sale = sequelize.define(
+        "sale",
+        {
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+                primaryKey: true,
+            },
+            productId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+            clientId: {
+                type: DataTypes.UUID,
+                allowNull: false,
+            },
+            saleDate: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+            quantity: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+
+            },
+        },
+        { timestamps: true }
+    );
+    return Sale;
+}
