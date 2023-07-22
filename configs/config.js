7dc2143e203a62d413c212ae29c7a90a3aa5be86
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-22 09:09:16 -0500

    chore(env-config): Updated settings for environment variables

--- configs/config.js
+++ configs/config.js
@@ -4,7 +4,11 @@ require("dotenv").config();
 const config = {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
-    
+    dbName: process.env.DB_NAME || "DB-XYZ",
+    dbUser: process.env.DB_USER || "root",
+    dbPassword: process.env.DB_PASSWORD || "root",
+    dbHos: process.env.DB_HOST || "127.0.0.1",
+    dbPort: process.env.DB_PORT || "3306",   
 };
 
 
