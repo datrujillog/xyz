2f3164ee0e2dcb5b00f29a628066dfb03b20f594
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-22 11:39:21 -0500

    feat(middleware-config): Added middlewares for morgan, express.json and express.urlencoded

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 //importar rutas
 const Product = require('./routes/product.Route');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 
 //utilizar rutas
 Product(app);
