69b528352fd84c5ff468b0563141a4ace3c93e21
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-22 12:06:14 -0500

    feat(service-method):Added createProduct method to create products

--- services/product.Service.js
+++ services/product.Service.js
@@ -1,12 +1,24 @@
 'use strict';
 
+const { ProductModel } = require("../database");
+
 class ProductService {
-    constructor() {
-        
-    }
-     async getProducts() {
-        return 'Hello World...'
-     }
+   constructor() {
+      // this.ProductModel = new ProductModel();
+
+
+   }
+   async getProducts() {
+      return 'Hello World...'
+   }
+
+   async createProduct(product) {
+
+      const createdProduct = await ProductModel.create(product);
+      return createdProduct;
+
+   }
+
 }
 
 module.exports = ProductService;
