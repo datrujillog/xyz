c1fa5c3d9bea1aaeb4564e158ebafcfe8f57cecf
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-22 10:40:38 -0500

    refactor(route-config) :Modified app.js to use Product routes

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ const express = require('express');
 
 const app = express();
 
+//importar rutas
+const Product = require('./routes/product.Route');
+
+//utilizar rutas
+Product(app);
+
 
 module.exports = app;
