713eb70dd3af5bbc14846a88e17817338bc4edf7
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-23 18:40:43 -0500

    feat(service-inventory): Added method to get the complete inventory of products in the inventory service

--- services/inventory.Service.js
+++ services/inventory.Service.js
@@ -6,19 +6,42 @@ const { ProductModel, InventoryModel } = require("../database");
 class InventoryService {
 
 
-    // GET /inventario: Obtener el inventario completo de productos en stock.
     async getProductsInventory() {
-
         const products = await ProductModel.findAll({ include: InventoryModel });
-        return products;
+        let totalStock = 0;
+        let totalVendidos = 0;
+
+        products.forEach((product) => {
+            totalStock += product.inventory.stock;
+            totalVendidos += product.inventory.solid;
+        });
+        return {
+            count: products.length,
+            totalStock,
+            totalVendidos,
+        };
+
     }
 
     async createProductInventory(product) {
 
         const createdProduct = await InventoryModel.create(product);
         return createdProduct;
-
     }
+
+    async invetoryById(id) {
+        try {
+
+            const inventory = await InventoryModel.findOne({ where: { productId: id } });
+            if (!inventory) {
+                throw new Error("There is no product with that id");
+            }
+            return inventory;
+        } catch (error) {
+            throw new Error(`${error.message}`);
+        }
+    }
+
 }
 
 module.exports = InventoryService;
