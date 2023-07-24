544c7713c6505c0039fa520841f72d4b3775cac2
Diego Trujillo <72278838+datrujillog@users.noreply.github.com>
2023-07-23 22:43:13 -0500

    feat(sales): Added route to get the best customers in the Sales route

---
+++ routes/sale.Route.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const SaleService = require('../services/sale.Service');
+
+function SaleRouter(app) {
+    const router = express.Router();
+    const saleServ = new SaleService();
+
+
+    app.use('/api/v1/sale', router);
+
+
+    router.get('/best-clients', async (req, res) => {
+        try {
+            const results = await saleServ.getTopCustomers();
+            res.json({
+                message: "Best clients",
+                results
+            });
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
+    });
+}
+module.exports = SaleRouter;
